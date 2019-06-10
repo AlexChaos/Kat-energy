1d27943da7bc0bd4abe035186e6082a743ff853d
Alex Chaos <the.airlight@gmail.com>
2019-06-10 16:47:03 +0300

    Собрана автоматизация проекта, добавлена адаптивная графика, доработана мобильная версия главного меню.

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,28 @@
-/*rjvtynfhbq*/
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var navClosed = document.querySelector('.main-nav__toggle--closed');
+var navOpened = document.querySelector('.main-nav__toggle--opened');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    if (navToggle.classList.contains('main-nav__toggle--closed')) {
+      navToggle.classList.remove('main-nav__toggle--closed');
+      navToggle.classList.add('main-nav__toggle--opened');
+    } else {
+      navToggle.classList.add('main-nav__toggle--closed');
+      navToggle.classList.remove('main-nav__toggle--opened');
+    }
+  });
+
+  navToggle.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
